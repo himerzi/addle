89d24cf3f1b04cd2e70274341dcfb99285d90f3c
Michael Detmold <michael.detmold@thatcrowdthing.com>
2013-08-16 15:27:44 +0100

    removed toms stuff

--- server.py
+++ server.py
@@ -9,28 +9,7 @@ render = web.template.render('templates/')
 
 class index:
     def GET(self, user_id):
-        return user_id
-
-        user_id = 'bob'
-
-        #list users followeds from api
-        followeds = ['mdetmold','tomchambers','joeroot','dclough','jordnb']
-        followeds_r = {
-            'mdetmold',['tomchambers','joeroot','johnnynobody','foghorn'],
-            'tomchambers',['mdetmold','joeroot','jordnb'],
-            'joeroot',['outofnetwork','beastboy','tomchambers','mdetmold','jordnb']
-        }
-
-        for a in followeds:
-            #list each followed's followeds from the api
-            followeds = followeds_r[a]
-            for b in followeds_r:
-                #perform check to see how many connections they have with the first level
-                #assign them a ranking
-
-        #weak_link = follower with lowest value
-
-        return weak_link
+        pass
 
 class twitter:
     def __init__(self):
