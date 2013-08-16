03d0af24993ff9c07fdf57d77802f5c99454def4
Tom Chambers <tom.chambers@gmail.com>
2013-08-16 11:49:19 +0100

    server file

---
+++ server.py
@@ -0,0 +1,35 @@
+import web
+import requests
+
+urls = (
+	'/(.*)', 'index'
+)
+
+render = web.template.render('templates/')
+
+class index:
+	def GET(self, user_id):
+		return user_id
+
+		user_id = 'bob'
+
+		#list users followeds from api
+		followeds = ['mdetmold','tomchambers','joeroot','dclough','jordnb']
+		followeds = {
+			'mdetmold',['tomchambers','joeroot','johnnynobody','foghorn'],
+			'tomchambers',['mdetmold','']
+		}
+
+		for a in followeds:
+			#list each followed's followeds from the api
+			for b in followeds_r:
+				#perform check to see how many connections they have with the first level
+				#assign them a ranking
+
+		#weak_link = follower with lowest value
+
+		return weak_link
+
+if __name__ == "__main__":
+	app = web.application(urls, globals())
+	app.run()
